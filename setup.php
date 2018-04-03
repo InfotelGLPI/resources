1eac84d626891bdf98b701182ec2d3b29dafcb99
amma35 <amandine.manceau@infotel.com>
2018-04-03 13:40:12 +0200

    update locales & version

--- setup.php
+++ setup.php
@@ -177,7 +177,7 @@ function plugin_version_resources() {
 
    return [
       'name'           => _n('Human Resource', 'Human Resources', 2, 'resources'),
-      'version'        => '2.4.1',
+      'version'        => '2.4.2',
       'license'        => 'GPLv2+',
       'author'         => "<a href='http://infotel.com/services/expertise-technique/glpi/'>Infotel</a>",
       'homepage'       => 'https://github.com/InfotelGLPI/resources',
