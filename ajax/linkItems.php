a1bfbe71b0ecb2a3c1351c485bbe4a126927845a
ales-infotel <alban.lesellier@infotel.com>
2020-12-07 14:31:02 +0100

    fix mistakes

--- ajax/linkItems.php
+++ ajax/linkItems.php
@@ -2,28 +2,28 @@
 /*
  * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
  -------------------------------------------------------------------------
- databases plugin for GLPI
- Copyright (C) 2009-2016 by the databases Development Team.
+ resources plugin for GLPI
+ Copyright (C) 2009-2016 by the resources Development Team.
 
- https://github.com/InfotelGLPI/databases
+ https://github.com/InfotelGLPI/resources
  -------------------------------------------------------------------------
 
  LICENSE
 
- This file is part of databases.
+ This file is part of resources.
 
- databases is free software; you can redistribute it and/or modify
+ resources is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
- databases is distributed in the hope that it will be useful,
+ resources is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
 
  You should have received a copy of the GNU General Public License
- along with databases. If not, see <http://www.gnu.org/licenses/>.
+ along with resources. If not, see <http://www.gnu.org/licenses/>.
  --------------------------------------------------------------------------
  */
 
