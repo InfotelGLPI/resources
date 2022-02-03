c0a5087aa4b2c028aea206b917cb8ae4fe8ead76
tsmr <thetsmr@gmail.com>
2022-02-03 18:18:56 +0100

    Fix -1 values

--- install/sql/update-3.0.0.sql
+++ install/sql/update-3.0.0.sql
@@ -10,3 +10,10 @@ ALTER TABLE `glpi_plugin_resources_resources` CHANGE `date_begin` `date_begin` t
 ALTER TABLE `glpi_plugin_resources_resources` CHANGE `date_end` `date_end` timestamp NULL DEFAULT NULL;
 ALTER TABLE `glpi_plugin_resources_resources` CHANGE `date_declaration_leaving` `date_declaration_leaving` timestamp NULL DEFAULT NULL;
 ALTER TABLE `glpi_plugin_resources_resources` CHANGE `date_mod` `date_mod` timestamp NULL DEFAULT NULL;
+
+UPDATE `glpi_plugin_resources_checklists` SET `plugin_resources_contracttypes_id` = '0' WHERE `plugin_resources_contracttypes_id` = '-1';
+UPDATE `glpi_plugin_resources_checklists` SET `plugin_resources_contracttypes_id` = '0' WHERE `plugin_resources_contracttypes_id` = '-1';
+UPDATE `glpi_plugin_resources_employees` SET `plugin_resources_clients_id` = '0' WHERE `plugin_resources_clients_id` = '-1';
+UPDATE `glpi_plugin_resources_resources` SET `plugin_resources_contracttypes_id` = '0' WHERE `plugin_resources_contracttypes_id` = '-1';
+UPDATE `glpi_plugin_resources_resources` SET `plugin_resources_teams_id` = '0' WHERE `plugin_resources_teams_id` = '-1';
+UPDATE `glpi_plugin_resources_resources` SET `users_id_sales` = '0' WHERE `users_id_sales` = '-1';
