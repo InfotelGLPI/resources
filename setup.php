4bea08f56268ec06de1d062dc8325ef570cdbdae
tsmr <thetsmr@gmail.com>
2022-06-27 09:15:58 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_RESOURCES_VERSION', '3.0.1');
+define('PLUGIN_RESOURCES_VERSION', '3.0.2');
 
 if (!defined("PLUGIN_RESOURCES_DIR")) {
    define("PLUGIN_RESOURCES_DIR", Plugin::getPhpDir("resources"));
