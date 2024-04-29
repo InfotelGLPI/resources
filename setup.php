0306d2644a72acb572dbf543cc912976ba1589be
ArthurMinfotel <arthur.mallet@infotel.com>
2024-04-29 16:14:14 +0200

    release 3.0.6

--- setup.php
+++ setup.php
@@ -27,7 +27,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_RESOURCES_VERSION', '3.0.5');
+define('PLUGIN_RESOURCES_VERSION', '3.0.6');
 
 if (!defined("PLUGIN_RESOURCES_DIR")) {
    define("PLUGIN_RESOURCES_DIR", Plugin::getPhpDir("resources"));
