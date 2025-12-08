5e2c70c4f9ad03718611c89a147ffa9c1f14b55c
Thetsmr <thetsmr@gmail.com>
2025-12-08 13:24:23 +0100

    Fix links with GLPI Bump version

--- hook.php
+++ hook.php
@@ -800,6 +800,8 @@ function plugin_resources_install()
         }
     }
 
+    $DB->runFile(PLUGIN_RESOURCES_DIR . "/install/sql/update-4.0.7.sql");
+
     $rep_files_resources = GLPI_PLUGIN_DOC_DIR . "/resources";
     if (!is_dir($rep_files_resources)) {
         mkdir($rep_files_resources);
---
+++ install/sql/update-4.0.7.sql
@@ -0,0 +1,2 @@
+UPDATE `glpi_savedsearches` SET `itemtype` = 'GlpiPlugin\\Resources\\Resource' WHERE `itemtype` = 'PluginResourcesResource';
+UPDATE `glpi_savedsearches_users` SET `itemtype` = 'GlpiPlugin\\Resources\\Resource' WHERE `itemtype` = 'PluginResourcesResource';
