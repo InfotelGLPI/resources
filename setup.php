0434cd3be6a0237a6ae56ee8c59e4a4d5bb45b89
Thetsmr <thetsmr@gmail.com>
2026-07-28 21:43:15 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -55,7 +55,7 @@ use GlpiPlugin\Resources\Servicecatalog;
 use GlpiPlugin\Resources\TaskPlanning;
 use GlpiPlugin\Reports\Report;
 
-define('PLUGIN_RESOURCES_VERSION', '4.0.15');
+define('PLUGIN_RESOURCES_VERSION', '4.0.16');
 
 global $CFG_GLPI;
 
